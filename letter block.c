ed53e262f1f7057312594f9b44e45fda0fbcb10e
KarinEzzat <24-101446@students.eui.edu.eg>
2025-02-17 19:44:13 +0200

    Add files via upload

---
+++ letter block.c
@@ -0,0 +1,12 @@
+  #include<iostream>
+using namespace std;
+int main(){
+ //cout<<"1 2 3 4 5"<<endl;
+ cout<<"  SSS    L"<<endl;
+ cout<<"S     S  L"<<endl;
+ cout<<"S        L"<<endl;
+ cout<<"  SSS    L"<<endl;
+ cout<<"      S  L"<<endl;
+ cout<<"S     S  L"<<endl;
+ cout<<"  SSS    LLLLL"<<endl;
+}
